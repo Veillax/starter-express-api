6321aa72f87234720d7dd2794120543ef6dbc06f
Veillax <126523099+Veillax@users.noreply.github.com>
2024-03-27 23:23:02 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors'); // Add cors library
 const cheerio = require('cheerio');
 const axios = require('axios');
 
 const app = express();
 const port = process.env.PORT || 3000; // Use environment variable for port
 
-const allowedOrigins = ['https://veillax.com', 'http://localhost:3000']; // Replace 3000 with your frontend port
-
-app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin) return callback(null, true); // Allow requests with no origin (localhost development)
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      return callback(null, true);
-    }
-    return callback(new Error('Origin not allowed by CORS'));
-  }
-}));
+// Enable CORS for all origins (for development)
+app.use(cors({ origin: true })); // Change origin to your website's URL for production
 
 app.get('/extract-urls', async (req, res) => {
   const albumUrl = req.query.url; // Get album URL from query parameter
